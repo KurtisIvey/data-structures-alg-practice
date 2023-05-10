9d6b105d30d1ef4af6a364f55453990ef4df5285
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-05-10 10:46:49 -0500

    add mergeSort

--- practice/maxSubarraySum.js
+++ practice/maxSubarraySum.js
@@ -32,7 +32,7 @@ function maxSubarraySum(arr, num) {
 console.log(maxSubarraySum([100, 200, 300, 400], 2));
  */
 
-function twoSum(arr, target) {
+/* function twoSum(arr, target) {
   if (arr.length === 0) return false;
   if (arr.length === 1 && arr[0] !== target) return false;
 
@@ -51,4 +51,43 @@ function twoSum(arr, target) {
   }
 }
 
-console.log(twoSum([-1, -2, -3, -4, -5], 6));
+console.log(twoSum([-1, -2, -3, -4, -5], 6)); */
+
+function merge(arr1, arr2) {
+  let left = 0;
+  let right = 0;
+  let sortedCombinedArr = [];
+
+  while (left < arr1.length && right < arr2.length) {
+    if (arr1[left] <= arr2[right]) {
+      sortedCombinedArr.push(arr1[left]);
+      left++;
+    }
+    if (arr2[right] < arr1[left]) {
+      sortedCombinedArr.push(arr2[right]);
+      right++;
+    }
+  }
+  while (left < arr1.length) {
+    sortedCombinedArr.push(arr1[left]);
+    left++;
+  }
+  while (right < arr2.length) {
+    sortedCombinedArr.push(arr2[right]);
+    right++;
+  }
+  return sortedCombinedArr;
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  //mergeSort()
+  // created mid point to guide slicing
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+}
+
+console.log(mergeSort([2002, 5, 92, 6, 123, 200]));
